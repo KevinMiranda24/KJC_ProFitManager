acca8c7781ac45a19bd04073dc720499177829cf
kevin <kevin@MIRANDA>
2025-09-27 23:46:53 -0600

    Agregar archivos de proyecto.

---
+++ KJC_ProFitManager/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace KJC_ProFitManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+			//Quitar siempre los Using no utilizados, click derecho, acciones rapidas, quitar usings no utilizados.
+		}
+	}
+}
